a0036c203887de0bc21e84042dcab1aee2e9b8a9
adamkanj <125460316+adamkanj@users.noreply.github.com>
2023-05-02 20:14:33 +0300

    Update Dijkstra_MPI.c

--- Dijkstra_MPI.c
+++ Dijkstra_MPI.c
@@ -1,113 +1,79 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <sys/time.h>
-#include "mpi.h"
-#define N 2048
-#define SOURCE 1
-#define MAXINT 9999999
-void SingleSource(int n, int source, int *wgt, int *lengths, MPI_Comm comm) {
-    int temp[N];
-    int i, j;
-    int nlocal; 
-    int *marker;
-    int firstvtx;
-    int lastvtx; 
-    int u, udist;
-    int lminpair[2], gminpair[2];
-    int npes, myrank;
-    MPI_Status status;
-    MPI_Comm_size(comm, &npes);
-    MPI_Comm_rank(comm, &myrank);
-    nlocal = n / npes;
-    firstvtx = myrank*nlocal;
-    lastvtx = firstvtx + nlocal - 1;
-    for (j = 0; j<nlocal; j++) {
-        lengths[j] = wgt[source*nlocal + j];
+#include <stdlib.h>
+#include <limits.h>
+#include <mpi.h>
+#define N 100
+#define INF INT_MAX
+int graph[N][N], dist[N], visited[N];
+int get_min_distance_vertex(int rank) {
+    int i, min = INF, min_index;
+    for (i = 0; i < N; i++) {
+        if (!visited[i] && dist[i] <= min) {
+            min = dist[i];
+            min_index = i;
+        }
+    }return min_index;
+}
+void init() {
+    int i;
+    for (i = 0; i < N; i++) {
+        dist[i] = INF;
+        visited[i] = 0;
     }
-    marker = (int *)malloc(nlocal*sizeof(int));
-    for (j = 0; j<nlocal; j++) {
-        marker[j] = 1;
+}
+void dijkstra(int rank, int num_procs) {
+    int i, j, u, start, end, num_vertices = 0;
+    if (rank == 0) {
+        start = 0;
+        end = N / num_procs;
+    } else if (rank == num_procs - 1) {
+        start = rank * (N / num_procs);
+        end = N;
+    } else {
+        start = rank * (N / num_procs);
+        end = start + (N / num_procs);
     }
-    if (source >= firstvtx && source <= lastvtx) {
-        marker[source - firstvtx] = 0;
+    for (i = start; i < end; i++) {
+        dist[i] = graph[0][i];
     }
-    for (i = 1; i<n; i++) {
-        lminpair[0] = MAXINT;
-        lminpair[1] = -1;
-        for (j = 0; j<nlocal; j++) {
-            if (marker[j] && lengths[j] < lminpair[0]) {
-                lminpair[0] = lengths[j];
-                lminpair[1] = firstvtx + j;
+    visited[0] = 1;
+    for (i = 0; i < N - 1; i++) {
+        u = get_min_distance_vertex(rank);
+        if (u >= start && u < end) {
+            visited[u] = 1;
+            num_vertices++;}
+        MPI_Bcast(&visited, N, MPI_INT, 0, MPI_COMM_WORLD);
+        for (j = start; j < end; j++) {
+            if (!visited[j] && graph[u][j] && dist[u] != INF
+                    && dist[u] + graph[u][j] < dist[j]) {
+                dist[j] = dist[u] + graph[u][j];
             }
         }
-        MPI_Allreduce(lminpair, gminpair, 1, MPI_2INT, MPI_MINLOC, comm);
-        udist = gminpair[0];
-        u = gminpair[1];
-        if (u == lminpair[1]) {
-            marker[u - firstvtx] = 0;
-        }
-        for (j = 0; j<nlocal; j++) {
-            if (marker[j] && ((udist + wgt[u*nlocal + j]) < lengths[j])) {
-                lengths[j] = udist + wgt[u*nlocal + j];
-            }
-        }
+    }int total_vertices;
+    MPI_Reduce(&num_vertices, &total_vertices, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    if (rank == 0) {
+        printf("Number of vertices: %d\n", N);
     }
-    free(marker);
 }
 int main(int argc, char *argv[]) {
-    int npes, myrank, nlocal;
-    int weight[N][N]; 
-    int distance[N]; 
-    int *localWeight; 
-    int *localDistance;
-    int sendbuf[N*N]; 
-    int i, j, k;
-    char fn[255];
-    FILE *fp;
-    double time_start, time_end;
-    struct timeval tv;
-    struct timezone tz;
-    gettimeofday(&tv, &tz);
-    time_start = (double)tv.tv_sec + (double)tv.tv_usec / 1000000.00;
+    int rank, num_procs, i, j;
     MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &npes);
-    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
-    nlocal = N/npes; 
-    localWeight = (int *)malloc(nlocal*N*sizeof(int));
-    localDistance = (int *)malloc(nlocal*sizeof(int));
-    if (myrank == SOURCE) {
-        strcpy(fn,"input2048.txt");
-        fp = fopen(fn,"r");
-        if ((fp = fopen(fn,"r")) == NULL) {
-            printf("Can't open the input file: %s\n\n", fn);
-            exit(1);
-        }
-        for(i = 0; i < N; i++) {
-            for(j = 0; j < N; j++) {
-                fscanf(fp,"%d", &weight[i][j]);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            if (i == j) {
+                graph[i][j] = 0;
+            } else {
+                graph[i][j] = rand() % 10;
             }
         }
-        for(k=0; k<npes; ++k) {
-            for(i=0; i<N;++i) {
-                for(j=0; j<nlocal;++j) {
-                    sendbuf[k*N*nlocal+i*nlocal+j]=weight[i][k*nlocal+j];
-                }
-            }
-        }
-    }
-    MPI_Scatter(sendbuf, nlocal*N, MPI_INT, localWeight, nlocal*N, MPI_INT, SOURCE, MPI_COMM_WORLD);
-    SingleSource(N, SOURCE, localWeight, localDistance, MPI_COMM_WORLD);
-    MPI_Gather(localDistance, nlocal, MPI_INT, distance, nlocal, MPI_INT, SOURCE, MPI_COMM_WORLD);
-    if (myrank == SOURCE) {
-        printf("Nodes: %d\n", N);
-        gettimeofday(&tv, &tz);
-        time_end = (double)tv.tv_sec + (double)tv.tv_usec / 1000000.00;
-        printf("time cost is %1f\n", time_end - time_start);
-    }
-    free(localWeight);
-    free(localDistance);
-    MPI_Finalize();
+    }init();
+    double start_time = MPI_Wtime();
+    dijkstra(rank, num_procs);
+    double end_time = MPI_Wtime();
+    if (rank == 0) {
+        printf("Runtime: %f ms\n", (end_time - start_time) * 1000);
+    }MPI_Finalize();
     return 0;
 }
